1901fe684bce6c6f7dc561aebd74c8b89c6e9c23
Frank Schmidt <frank.schmidt@codecentric.de>
2019-12-04 22:21:30 +0100

    Day 4 part 1 using Rust

---
+++ src/bin/day04.rs
@@ -0,0 +1,61 @@
+use std::collections::HashSet;
+//use std::collections::Vec;
+
+fn num2vec(mut num: i32) -> Vec<i32> {
+    let mut v = Vec::new();
+    while num > 0 {
+        v.push(num%10);
+        num = num / 10;
+    }
+    v.reverse();
+    return v;
+}
+fn vec2num(vec: &Vec<i32>) -> i32 {
+    let mut num = 0;
+    for d in vec {
+        num *= 10;
+        num += d;
+    }
+    return num;
+}
+
+fn iterate_possible_passwords<F>(num_digits: i32, digits: &mut Vec<i32>, correct: &F) -> i32  where F:Fn(Vec<i32>) -> bool {
+    if num_digits <= 0 {
+        if correct(digits.to_vec()) {
+            println!("{:?}",vec2num(digits));
+            return 1;
+        }
+        return 0;
+    }
+    let mut sum = 0;
+    for i in 1..10 {
+        digits.push(i);
+        sum += iterate_possible_passwords(num_digits - 1, digits, correct);
+        digits.pop();
+    }
+    return sum;
+}
+
+fn correct_1(digits: &Vec<i32>) -> bool {
+    let mut last = 0;
+    let mut double = false;
+    for d in digits {
+        if *d < last {return false;}
+        double = double || *d == last;
+        last = *d
+    }
+    return double
+}
+
+fn main() {
+    // x <= x+1, 1 double, double!=triple
+    //digits2num(x) < 172851 or digits2num(x) > 675869
+    println!("{:?}",num2vec(123456));
+    println!("{:?}",vec2num(&num2vec(123456)));
+    println!("{:?}", (1..10).map(|x| x));
+
+    println!("{}",iterate_possible_passwords(2, &mut Vec::new(), &|x| vec2num(&x) % 10 == 1));
+    println!("day 4 part 1: {}", iterate_possible_passwords(6, &mut Vec::new(), &|x| vec2num(&x) > 172851 && vec2num(&x) < 675869 && correct_1(&x)));
+
+    println!("day 4 part 2: {}", 0);
+}
