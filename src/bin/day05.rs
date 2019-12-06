88583bc743f5c42c7fad69e5024038059fa18bad
Frank Schmidt <frank.schmidt@codecentric.de>
2019-12-07 00:26:23 +0100

    Refactor intcode to use state object

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -5,7 +5,6 @@ use std::fs;
 
 fn main() {
     let line = fs::read_to_string("day05").unwrap();
-    let mut memory: Vec<i32> = line.split(",").map(|x| x.trim()).map(|x| x.parse().unwrap()).collect();
-    intcode::run_until_halt(&mut memory);
-    println!("{:?}", memory)
+    let memory: Vec<i32> = line.split(",").map(|x| x.trim()).map(|x| x.parse().unwrap()).collect();
+    intcode::run_until_halt(memory, vec![1]);
 }
--- src/intcode.rs
+++ src/intcode.rs
@@ -1,12 +1,9 @@
 use std::collections::HashMap;
-use std::io;
 
-pub fn test() {
-    println!("import intcode worked")
-}
-
-fn indir(memory: &Vec<i32>, ip: usize) -> usize{
-    return memory[ip] as usize;
+pub struct IntCode {
+    mem: Vec<i32>,
+    ip: usize,
+    input: Vec<i32>,
 }
 
 fn handle_flags(memory: &Vec<i32>, ip: usize, pos: u32, flags:i32) -> i32 {
@@ -19,49 +16,49 @@ fn handle_flags(memory: &Vec<i32>, ip: usize, pos: u32, flags:i32) -> i32 {
     }
 }
 
-//trait StepFunction: Fn(&mut Vec<i32>, usize, i32) -> Option<usize>;
-
-pub fn perform_step(mut memory: &mut Vec<i32>, ip: &mut usize, op_map: &HashMap<i32, Box<dyn Fn(&mut Vec<i32>, usize, i32) -> Option<usize>>>) -> bool {
-    let op = memory[*ip] % 100;
-    let flags = memory[*ip] / 100;
-    //println!("before {} {:?}", op, memory);
-    let delta_ip = op_map[&op](&mut memory, *ip, flags);
-    //println!("after {:?}", memory);
-    if delta_ip.is_none() {
-        return false; // break
-    }
-    *ip += delta_ip.unwrap();
-    return true; // continue
-}
-
-fn input() -> i32 {
+fn input(state: &mut IntCode) -> i32 {
     //let mut line = String::new();
     //let stdin = io::stdin();
     //stdin.lock().read_line(&mut line).expect("Could not read line");
     //println!("{}", line);
-    return 1;
+    return state.input.pop().unwrap()
 }
 fn output(value: i32) {
     print!("{}", value);
 }
 
-pub fn run_until_halt(mut memory: &mut Vec<i32>){//, op_map: HashMap<i32, Box<dyn Fn(&mut Vec<i32>, usize) -> Option<usize>>>){
-    let mut op_map: HashMap<i32, Box<dyn Fn(&mut Vec<i32>, usize, i32) -> Option<usize>>> = HashMap::new();
-    op_map.insert(1, Box::new(|mem, ip, flags| {
-        let target = mem[ip+3] as usize;
-        mem[target] = handle_flags(&mem, ip, 1, flags) + handle_flags(&mem, ip, 2, flags); return Some(4);
-    }));
-    op_map.insert(2, Box::new(|mem, ip, flags| {
-        let target = mem[ip+3] as usize;
-        mem[target] = handle_flags(&mem, ip, 1, flags) * handle_flags(&mem, ip, 2, flags); return Some(4);
-    }));
-    op_map.insert(3, Box::new(|mem, ip, flags| { let target = mem[ip+1] as usize; mem[target] = input(); Some(2) } ));
-    op_map.insert(4, Box::new(|mem, ip, flags| { output(handle_flags(&mem, ip, 1, flags)); Some(2) }));
-    op_map.insert(99, Box::new(|_, _, _| None ));
+//trait StepFunction: Fn(&mut Vec<i32>, usize, i32) -> Option<usize>;
 
-    let mut ip = 0;
-    while perform_step(&mut memory, &mut ip, &op_map) {
+pub fn perform_step(state: &mut IntCode, op_map: &HashMap<i32, Box<dyn Fn(&mut IntCode, i32) -> Option<usize>>>) -> bool {
+    let op = state.mem[state.ip] % 100;
+    let flags = state.mem[state.ip] / 100;
+    //println!("before {} {:?}", op, memory);
+    let delta_ip = op_map[&op](state, flags);
+    //println!("after {:?}", memory);
+    if delta_ip.is_none() {
+        return false; // break
+    }
+    state.ip += delta_ip.unwrap();
+    return true; // continue
+}
+
+pub fn run_until_halt(memory: Vec<i32>, input_buffer: Vec<i32>){
+    let mut state = IntCode{mem: memory, ip: 0, input: input_buffer};
+    let mut op_map: HashMap<i32, Box<dyn Fn(&mut IntCode, i32) -> Option<usize>>> = HashMap::new();
+    op_map.insert(1, Box::new(|state, flags| {
+        let target = state.mem[state.ip+3] as usize;
+        state.mem[target] = handle_flags(&state.mem, state.ip, 1, flags) + handle_flags(&state.mem, state.ip, 2, flags); return Some(4);
+    }));
+    op_map.insert(2, Box::new(|state, flags| {
+        let target = state.mem[state.ip+3] as usize;
+        state.mem[target] = handle_flags(&state.mem, state.ip, 1, flags) * handle_flags(&state.mem, state.ip, 2, flags); return Some(4);
+    }));
+    op_map.insert(3, Box::new(|state, flags| { let target = state.mem[state.ip+1] as usize; state.mem[target] = input(state); Some(2) } ));
+    op_map.insert(4, Box::new(|state, flags| { output(handle_flags(&state.mem, state.ip, 1, flags)); Some(2) }));
+    op_map.insert(99, Box::new(|_, _| None ));
+
+    while perform_step(&mut state, &op_map) {
     }
     println!("");
-    println!("result: {}", memory[0]);
+    println!("result: {}", state.mem[0]);
 }
